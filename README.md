b4f6c887a52c42b43d49f4876c5581fa92ef8b54
Mateus Artur <artur.corseg@gmail.com>
2022-12-16 13:15:33 -0300

    Projeto bloco de notas v1

---
+++ README.md
@@ -0,0 +1,11 @@
+<h2>Bloco de notas</h2>
+
+<p>Projeto criado como forma de treinar e desenvolver conforme aprendo novos conceitos</P>
+
+<h4>1° parte - HTML & CSS - concluído</h4>
+<br>
+<br>
+<h4>2º parte - Lógica do javaScript preparada - concluído</h4>
+<br>
+<br>
+<h4>Loading. . .</h4>
---
+++ main.js
@@ -0,0 +1,53 @@
+//variaveis
+
+
+//Deixar o botão adicionar anotação clicavel
+
+
+/*Quando clicar no botao abrir um formulario com dois inputs, 
+um text e outro textarea e armazenar os valores dos iput em variavel*/
+
+
+//Criar um elemento Div com a class anotacao
+
+
+//Inserir os valores capturados do input no h2 e paragrafo da anotação
+
+
+//Agora queremos fazer algo com a anotação]
+
+/*Ao clicar na anotação teremos três opções, 
+   - Visualizar
+   - Adicionar a uma pasta
+   - Excluir 
+*/
+
+//Vamos visualizar, primeiramente
+
+/*quando clicar na anotação aparecerá um menu com a opção visualizar, e ao clicar sobre ela irá esconder
+ o item de adicionar anotação com a class esconde-item e troca a class da anotacao por visualizar-anotacao*/
+
+
+
+ //para retornar a class normal da anotação adicionar um botao fechar na parte debaixo da anotação
+ //Ao clicar nesse botao, a class visualizar-anotacao será substituida pela class anotacao
+ //tambem será trocado a classs do botao de adicionar, voltará a class botao-adiciona-nota
+
+
+ //Agora vamos adicionar a uma pasta
+//Primeiro criaremos um array das pastas
+
+ /*Se clicado no botao de adicionar a uma pasta, será verificado se já existe alguma pasta
+se existir, aparecerão os nomes em formato de botao clicavel no form, se clicado no botão
+será buscado o array desejado dento do array de arrays, e  será adicionado o elemento em 
+uma posicao desse array desejado.
+    Se não tiver a pasta que deseja adicionar, terá um botão de adicionar pasta, esse abribrá um input
+perguntando o nome da pasta, ao colocar e salvar, apasta aparecerá na lista de pastas, e seguirá a 
+mesma lógica de adicionar ao array desejado*/
+
+//Agora vamos excluir a anotacao
+
+//No menu, ao clicar em excluir, será consultado o elemento por meio de variavel e usando o remove será excluido
+
+
+//caso deseja cancelar a ação, se clicar em qualquer lugar da tela diferente do elemento ele será desselecionado
